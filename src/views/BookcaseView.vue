35d267c211215973d5f70151bdbef79bd252176c
yanyan <feilichen@yeah.net>
2022-08-14 23:50:23 +0800

    feat

--- src/views/BookcaseView.vue
+++ src/views/BookcaseView.vue
@@ -2,18 +2,29 @@
     <div class="bookcase-view">
         <SelectBar class="left-bar hidden-scrollbar" topNav="书柜文件夹" :navList="navList"></SelectBar>
         <div class="right-content">
-            <div class="filter-wrapper">
-                <div class="read-button button-wrapper">
-                    <i class="iconfont icon-read"></i>
+            <div class="top-wrapper">
+                <div class="operation-wrapper">
+                    <div class="button confirm"></div>
+                    <div class="button cancel"></div>
                 </div>
-                <div class="hot-button button-wrapper selected">
-                    <i class="iconfont icon-hot"></i>
+                <div class="filter-wrapper">
+                    <div class="read-button button-wrapper">
+                        <i class="iconfont icon-remove"></i>
+                    </div>
+                    <div class="hot-button button-wrapper selected">
+                        <i class="iconfont icon-delete"></i>
+                    </div>
+                    <div class="like-button button-wrapper">
+                        <!-- icon-tile -->
+                        <i class="iconfont icon-list"></i>
+                    </div>
+                    <div class="filter-button button-wrapper selected">
+                        <i class="iconfont icon-up"></i>
+                    </div>
                 </div>
-                <div class="like-button button-wrapper">
-                    <i class="iconfont icon-like"></i>
-                </div>
-                <div class="filter-button button-wrapper selected">
-                    <i class="iconfont icon-up"></i>
+                <div class="create-folder">
+                    <span class="icon">+</span>
+                    <span class="text">新建文件夹</span>
                 </div>
             </div>
             <div class="book-wrapper">
@@ -72,40 +83,119 @@
             margin-left: 30px;
             width: calc(100% - 250px);
 
-            .filter-wrapper {
+            .top-wrapper {
                 position: relative;
-                display: flex;
-                align-items: center;
-                justify-content: center;
-                height: 48px;
                 margin: 0 30px 20px 0;
 
-                .button-wrapper {
+                .operation-wrapper {
+                    position: absolute;
+                    top: 0;
+                    left: 0;
+
+                    .button {
+                        position: relative;
+                        display: inline-block;
+                        width: 76px;
+                        height: 48px;
+                        background: #FFFFFF;
+                        border-radius: 24px;
+                        cursor: pointer;
+
+                        &.confirm {
+                            margin-right: 10px;
+
+                            &::before {
+                                content: '确定';
+                                position: absolute;
+                                top: 4px;
+                                left: 4px;
+                                width: 68px;
+                                height: 40px;
+                                line-height: 40px;
+                                text-align: center;
+                                color: #071327;
+                                font-weight: bold;
+                                border-radius: 24px;
+                                background: var(--hoverColor);
+                                z-index: 6;
+                            }
+                        }
+
+                        &.cancel::before {
+                            content: '取消';
+                            position: absolute;
+                            top: 4px;
+                            left: 4px;
+                            width: 68px;
+                            height: 40px;
+                            line-height: 40px;
+                            text-align: center;
+                            color: #071327;
+                            font-weight: bold;
+                            border-radius: 24px;
+                            background: var(--hoverColor);
+                            z-index: 6;
+                        }
+                    }
+                }
+
+                .filter-wrapper {
+                    position: relative;
                     display: flex;
                     align-items: center;
                     justify-content: center;
-                    width: 36px;
-                    height: 36px;
-                    border-radius: 10px;
-                    margin-right: 10px;
-                    background: #FFFFFF;
-                    border: 1px solid transparent;
-                    cursor: pointer;
+                    height: 48px;
 
-                    .iconfont {
-                        color: #0B172A;
-                    }
+                    .button-wrapper {
+                        display: flex;
+                        align-items: center;
+                        justify-content: center;
+                        width: 36px;
+                        height: 36px;
+                        border-radius: 10px;
+                        margin-right: 10px;
+                        background: #FFFFFF;
+                        border: 1px solid transparent;
+                        cursor: pointer;
 
-                    &:hover,
-                    &.selected {
-                        border-color: var(--mainColor);
                         .iconfont {
-                            color: var(--mainColor);
+                            color: #0B172A;
+                        }
+
+                        &:hover,
+                        &.selected {
+                            border-color: var(--mainColor);
+                            .iconfont {
+                                color: var(--mainColor);
+                            }
+                        }
+
+                        &:last-child {
+                            margin-right: 0;
                         }
                     }
+                }
 
-                    &:last-child {
-                        margin-right: 0;
+                .create-folder {
+                    position: absolute;
+                    top: 0;
+                    right: 0;
+                    display: flex;
+                    align-items: center;
+                    height: 48px;
+                    color: #FFFFFF;
+                    border-radius: 16px;
+                    padding: 0 24px;
+                    background: var(--mainColor);
+                    box-shadow: 2px 6px 8px var(--activeColor);
+                    cursor: pointer;
+
+                    .icon {
+                        font-size: 20px;
+                    }
+
+                    .text {
+                        margin-left: 10px;
                     }
                 }
             }
